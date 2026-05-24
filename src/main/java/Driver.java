35f8ac4c80c643818bcab02afb09a321d3830749
fadliachsan27 <fadliachsan010@gmail.com>
2026-05-24 14:02:53 +0700

    Update Driver2.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -8,11 +8,11 @@ public class Driver {
 		
 		System.out.println("Current count:" + counter.getCount());
 		
-		counter.increment(2);
+		counter.increment();
 		
 		System.out.println("Current count:" + counter.getCount());
 		
-		counter.decrement(5);
+		counter.decrement();
 		
 		System.out.println("Current count:" + counter.getCount());
 		
